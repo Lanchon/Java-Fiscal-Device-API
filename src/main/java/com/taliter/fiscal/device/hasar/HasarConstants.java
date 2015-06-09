ec0f469c28011cf22302aa382044ff54dbd8e787
Lanchon <lanchon@gmail.com>
2015-06-09 13:14:53 -0300

    Ported the project to Gradle

--- src/main/java/com/taliter/fiscal/device/hasar/HasarConstants.java
+++ src/main/java/com/taliter/fiscal/device/hasar/HasarConstants.java
@@ -5,13 +5,13 @@ public interface HasarConstants
 {
 	//// CODIGOS DE COMANDO //////////////////
 
-	// Comandos de configuraciÛn y consulta
+	// Comandos de configuraci√≥n y consulta
 
 	/** Consultar estado. (0x2A) */
 	public int CMD_STATUS_REQUEST             = 0x2A;
 	/** Consultar estado intermedio. (0xA1) */
 	public int CMD_STATPRN                    = 0xA1;
-	/** Consultar versiÛn del controlador fiscal. (0x7F) */
+	/** Consultar versi√≥n del controlador fiscal. (0x7F) */
 	public int CMD_GET_FISCAL_DEVICE_VERSION  = 0x7F;
 
 	///** Dar formato a la memoria fiscal. (0x--) */
@@ -21,7 +21,7 @@ public interface HasarConstants
 	/** Dar de baja la memoria fiscal. (0xB1) */
 	public int CMD_KILL_FISCAL_MEMORY         = 0xB1;
 
-	/** Cambiar velocidad de comunicaciÛn. (0xA0) */
+	/** Cambiar velocidad de comunicaci√≥n. (0xA0) */
 	public int CMD_SET_BAUD_RATE              = 0xA0;
 
 	/** Cambiar fecha y hora. (0x58) */
@@ -29,27 +29,27 @@ public interface HasarConstants
 	/** Consultar fecha y hora. (0x59) */
 	public int CMD_GET_DATE_TIME              = 0x59;
 
-	/** Cargar configuraciÛn, comando nuevo. (0x95) */
+	/** Cargar configuraci√≥n, comando nuevo. (0x95) */
 	public int CMD_SET_GENERAL_CONFIGURATION  = 0x95;
-	/** Consultar configuraciÛn, comando nuevo. (0x96) */
+	/** Consultar configuraci√≥n, comando nuevo. (0x96) */
 	public int CMD_GET_GENERAL_CONFIGURATION  = 0x96;
-	/** Cargar configuraciÛn, comando viejo. (0x65) */
+	/** Cargar configuraci√≥n, comando viejo. (0x65) */
 	public int CMD_SET_CONFIGURATION_BY_BLOCK = 0x65;
-	/** Consultar configuraciÛn, comando viejo. (0x66) */
+	/** Consultar configuraci√≥n, comando viejo. (0x66) */
 	public int CMD_GET_CONFIGURATION_BY_BLOCK = 0x66;
-	/** Cargar configuraciÛn por par·metro. (0x64) */
+	/** Cargar configuraci√≥n por par√°metro. (0x64) */
 	public int CMD_SET_CONFIGURATION_BY_ONE   = 0x64;
 
-	/** Consultar datos de inicializaciÛn. (0x73) */
+	/** Consultar datos de inicializaci√≥n. (0x73) */
 	public int CMD_GET_INIT_DATA              = 0x73;
 	/** Cambiar responsabilidad frente al IVA. (0x63) */
 	public int CMD_CHANGE_IVA_RESPONSABILITY  = 0x63;
-	/** Cambiar n˙mero de Ingresos Brutos. (0x6E) */
+	/** Cambiar n√∫mero de Ingresos Brutos. (0x6E) */
 	public int CMD_CHANGE_IB_NUMBER           = 0x6E;
 
-	/** Cargar nombre de fantasÌa del propietario. (0x5F) */
+	/** Cargar nombre de fantas√≠a del propietario. (0x5F) */
 	public int CMD_SET_FANTASY_NAME           = 0x5F;
-	/** Consultar nombre de fantasÌa del propietario. (0x92) */
+	/** Consultar nombre de fantas√≠a del propietario. (0x92) */
 	public int CMD_GET_FANTASY_NAME           = 0x92;
 
 	/** Cargar encabezamiento y pie de documentos. (0x5D) */
@@ -69,17 +69,17 @@ public interface HasarConstants
 	public int CMD_GET_HISTORY_CAPACITY       = 0x37;
 	/** Cerrar jornada fiscal imprimiendo reporte. (0x39) */
 	public int CMD_DAILY_CLOSE                = 0x39;
-	/** Imprimir reporte de auditorÌa por fecha. (0x3A) */
+	/** Imprimir reporte de auditor√≠a por fecha. (0x3A) */
 	public int CMD_DAILY_CLOSE_BY_DATE        = 0x3A;
-	/** Imprimir reporte de auditorÌa por n˙mero de Z. (0x3B) */
+	/** Imprimir reporte de auditor√≠a por n√∫mero de Z. (0x3B) */
 	public int CMD_DAILY_CLOSE_BY_NUMBER      = 0x3B;
 	/** Consultar registro diario. (0x3C) */
 	public int CMD_GET_DAILY_REPORT           = 0x3C;
 	/** Consultar memoria de trabajo. (0x67) */
 	public int CMD_GET_WORKING_MEMORY         = 0x67;
-	/** Iniciar consulta de la informaciÛn de IVA. (0x70) */
+	/** Iniciar consulta de la informaci√≥n de IVA. (0x70) */
 	public int CMD_GET_FIRST_IVA_DATA         = 0x70;
-	/** Continuar consulta de la informaciÛn de IVA. (0x71) */
+	/** Continuar consulta de la informaci√≥n de IVA. (0x71) */
 	public int CMD_GET_NEXT_IVA_DATA          = 0x71;
 
 
@@ -87,21 +87,21 @@ public interface HasarConstants
 
 	/** Cancelar documento. (0x98) */
 	public int CMD_CANCEL_DOCUMENT            = 0x98;
-	/** Reimprimir el ˙ltimo documento emitido. (0x99) */
+	/** Reimprimir el √∫ltimo documento emitido. (0x99) */
 	public int CMD_REPRINT_DOCUMENT           = 0x99;
-	/** Cargar cÛdigo de barras. (0x5A) */
+	/** Cargar c√≥digo de barras. (0x5A) */
 	public int CMD_SET_BAR_CODE               = 0x5A;
 	/** Cargar datos del cliente. (0x62) */
 	public int CMD_SET_CUSTOMER_DATA          = 0x62;
-	/** Cargar informaciÛn de remito o de comprobante original. (0x93) */
+	/** Cargar informaci√≥n de remito o de comprobante original. (0x93) */
 	public int CMD_SET_EMBARK_NUMBER          = 0x93;
-	/** Consultar informaciÛn de remito o de comprobante original. (0x94) */
+	/** Consultar informaci√≥n de remito o de comprobante original. (0x94) */
 	public int CMD_GET_EMBARK_NUMBER          = 0x94;
 	/** Avanzar papel de ticket. (0x50) */
 	public int CMD_FEED_TICKET                = 0x50;
-	/** Avanzar papel de auditorÌa. (0x51) */
+	/** Avanzar papel de auditor√≠a. (0x51) */
 	public int CMD_FEED_JOURNAL               = 0x51;
-	/** Avanzar papeles de ticket y auditorÌa. (0x52) */
+	/** Avanzar papeles de ticket y auditor√≠a. (0x52) */
 	public int CMD_FEED_TICKET_AND_JOURNAL    = 0x52;
 
 
@@ -111,13 +111,13 @@ public interface HasarConstants
 	public int CMD_OPEN_FD                    = 0x40;
 	/** DF: Imprimir texto fiscal. (0x41) */
 	public int CMD_PRINT_FISCAL_TEXT          = 0x41;
-	/** DF: Imprimir Ìtem. (0x42) */
+	/** DF: Imprimir √≠tem. (0x42) */
 	public int CMD_PRINT_LINE_ITEM            = 0x42;
-	/** DF: Descuento o recargo sobre el ˙ltimo Ìtem. (0x55) */
+	/** DF: Descuento o recargo sobre el √∫ltimo √≠tem. (0x55) */
 	public int CMD_LAST_ITEM_DISCOUNT         = 0x55;
 	/** DF: Descuento general. (0x54) */
 	public int CMD_GENERAL_DISCOUNT           = 0x54;
-	/** DF: DevoluciÛn de envases, bonificaciones y recargos. (0x6D) */
+	/** DF: Devoluci√≥n de envases, bonificaciones y recargos. (0x6D) */
 	public int CMD_RETURN_RECHARGE            = 0x6D;
 	/** DF: Recargo de IVA a responsable no inscripto. (0x61) */
 	public int CMD_CHARGE_NON_REGISTERED_TAX  = 0x61;
@@ -125,7 +125,7 @@ public interface HasarConstants
 	public int CMD_PERCEPTIONS                = 0x60;
 	/** DF: Consultar subtotal. (0x43) */
 	public int CMD_SUBTOTAL                   = 0x43;
-	/** DF: Definir lÌneas de texto en recibos. (0x97) */
+	/** DF: Definir l√≠neas de texto en recibos. (0x97) */
 	public int CMD_RECEIPT_TEXT               = 0x97;
 	/** DF: Imprimir total y pago. (0x44) */
 	public int CMD_TOTAL_TENDER               = 0x44;
@@ -151,11 +151,11 @@ public interface HasarConstants
 
 	/** DNFH: Abrir documento no fiscal homologado. (0x80) */
 	public int CMD_OPEN_NFHD                  = 0x80;
-	/** DNFH: Imprimir Ìtem en remito u orden de salida. (0x82) */
+	/** DNFH: Imprimir √≠tem en remito u orden de salida. (0x82) */
 	public int CMD_PRINT_EMBARK_ITEM          = 0x82;
-	/** DNFH: Imprimir Ìtem en resumen de cuenta o en cargo a la habitaciÛn. (0x83) */
+	/** DNFH: Imprimir √≠tem en resumen de cuenta o en cargo a la habitaci√≥n. (0x83) */
 	public int CMD_PRINT_ACCOUNT_ITEM         = 0x83;
-	/** DNFH: Imprimir Ìtem en cotizaciÛn. (0x84) */
+	/** DNFH: Imprimir √≠tem en cotizaci√≥n. (0x84) */
 	public int CMD_PRINT_QUOTATION_ITEM       = 0x84;
 	/** DNFH: Cerrar documento no fiscal homologado. (0x81) */
 	public int CMD_CLOSE_NFHD                 = 0x81;
@@ -177,7 +177,7 @@ public interface HasarConstants
 
 	// Otros comandos
 
-	/** Abrir cajÛn de dinero. (0x7B) */
+	/** Abrir caj√≥n de dinero. (0x7B) */
 	public int CMD_OPEN_DRAWER                = 0x7B;
 	/** Escribir en visor. (0xB2) */
 	public int CMD_WRITE_DISPLAY              = 0xB2;
@@ -191,10 +191,10 @@ public interface HasarConstants
 	//public int PST_BIT_1                      = 0x0002;
 
 	/** Error de impresora. (Bit 2 del estado de impresora) */
-	// TambiÈn puede seÒalar impresora ocupada en impresoras para Chile.
+	// Tambi√©n puede se√±alar impresora ocupada en impresoras para Chile.
 	public int PST_PRINTER_ERROR              = 0x0004;
 
-	/** Impresora fuera de lÌnea. No ha podido comunicarse con la impresora dentro del perÌodo de tiempo establecido. (Bit 3 del estado de impresora) */
+	/** Impresora fuera de l√≠nea. No ha podido comunicarse con la impresora dentro del per√≠odo de tiempo establecido. (Bit 3 del estado de impresora) */
 	public int PST_PRINTER_OFFLINE            = 0x0008;
 
 	/** Falta papel del diario. (Bit 4 del estado de impresora) */
@@ -203,10 +203,10 @@ public interface HasarConstants
 	/** Falta papel de tickets. (Bit 5 del estado de impresora) */
 	public int PST_TICKET_PAPER_OUT           = 0x0020;
 
-	/** Buffer de impresiÛn lleno. Cualquier comando que se envÌe cuando este bit est· en 1 no se ejecuta y debe ser reenviado por la aplicaciÛn. (Bit 6 del estado de impresora) */
+	/** Buffer de impresi√≥n lleno. Cualquier comando que se env√≠e cuando este bit est√° en 1 no se ejecuta y debe ser reenviado por la aplicaci√≥n. (Bit 6 del estado de impresora) */
 	public int PST_PRINT_BUFFER_FULL          = 0x0040;
 
-	/** Buffer de impresiÛn vacÌo. Todos los comandos fueron enviados a la impresora. (Bit 7 del estado de impresora) */
+	/** Buffer de impresi√≥n vac√≠o. Todos los comandos fueron enviados a la impresora. (Bit 7 del estado de impresora) */
 	public int PST_PRINT_BUFFER_EMPTY         = 0x0080;
 
 	/** Tapa de impresora abierta. (Bit 8 del estado de impresora) */
@@ -222,19 +222,19 @@ public interface HasarConstants
 
 	//public int PST_BIT_13                     = 0x2000;
 
-	/** CajÛn de dinero cerrado o ausente. (Bit 14 del estado de impresora) */
+	/** Caj√≥n de dinero cerrado o ausente. (Bit 14 del estado de impresora) */
 	public int PST_MONEY_DRAWER_CLOSED        = 0x4000;
 
-	/** Suma lÛgica (OR) de los bits 2 a 5, 8 y 14. Este bit se encuentra en 1 siempre que alguno de los bits mencionados se encuentre en 1. (Bit 15 del estado de impresora) */
+	/** Suma l√≥gica (OR) de los bits 2 a 5, 8 y 14. Este bit se encuentra en 1 siempre que alguno de los bits mencionados se encuentre en 1. (Bit 15 del estado de impresora) */
 	public int PST_BITWISE_OR                 = 0x8000;
 
 
 	//// ESTADO FISCAL ///////////////////////
 
-	/** Error en chequeo de memoria fiscal. Al encenderse la impresora se produjo un error en el checksum. La impresora no funcionar·. (Bit 0 de estado fiscal) */
+	/** Error en chequeo de memoria fiscal. Al encenderse la impresora se produjo un error en el checksum. La impresora no funcionar√°. (Bit 0 de estado fiscal) */
 	public int FST_FISCAL_MEMORY_CRC_ERROR    = 0x0001;
 
-	/** Error en chequeo de memoria de trabajo. Al encenderse la impresora se produjo un error en el checksum. La impresora no funcionar·. (Bit 1 de estado fiscal) */
+	/** Error en chequeo de memoria de trabajo. Al encenderse la impresora se produjo un error en el checksum. La impresora no funcionar√°. (Bit 1 de estado fiscal) */
 	public int FST_WORKING_MEMORY_CRC_ERROR   = 0x0002;
 
 	//public int FST_BIT_2                      = 0x0004;
@@ -242,19 +242,19 @@ public interface HasarConstants
 	/** Comando desconocido. El comando recibido no fue reconocido. (Bit 3 de estado fiscal) */
 	public int FST_UNKNOWN_COMMAND            = 0x0008;
 
-	/** Datos inv·lidos en un campo. Uno de los campos del comando recibido tiene datos no v·lidos (por ejemplo, datos no numÈricos en un campo numÈrico). (Bit 4 de estado fiscal) */
+	/** Datos inv√°lidos en un campo. Uno de los campos del comando recibido tiene datos no v√°lidos (por ejemplo, datos no num√©ricos en un campo num√©rico). (Bit 4 de estado fiscal) */
 	public int FST_INVALID_DATA_FIELD         = 0x0010;
 
-	/** Comando inv·lido para el estado fiscal actual. Se ha recibido un comando que no es v·lido en el estado actual del controlador (por ejemplo, abrir un documento no fiscal cuando se encuentra abierto un documento fiscal). (Bit 5 de estado fiscal) */
+	/** Comando inv√°lido para el estado fiscal actual. Se ha recibido un comando que no es v√°lido en el estado actual del controlador (por ejemplo, abrir un documento no fiscal cuando se encuentra abierto un documento fiscal). (Bit 5 de estado fiscal) */
 	public int FST_INVALID_COMMAND            = 0x0020;
 
-	/** Desborde de acumulador. El acumulador de una transacciÛn, del total diario o del IVA se desbordar· a raÌz de un comando recibido. El comando no es ejecutado. Este bit debe ser monitoreado por la aplicaciÛn para emitir el correspondiente aviso. (Bit 6 de estado fiscal) */
+	/** Desborde de acumulador. El acumulador de una transacci√≥n, del total diario o del IVA se desbordar√° a ra√≠z de un comando recibido. El comando no es ejecutado. Este bit debe ser monitoreado por la aplicaci√≥n para emitir el correspondiente aviso. (Bit 6 de estado fiscal) */
 	public int FST_ACCUMULATOR_OVERFLOW       = 0x0040;
 
 	/** Memoria fiscal llena, bloqueada o dada de baja. No se permite abrir un comprobante fiscal. (Bit 7 de estado fiscal) */
 	public int FST_FISCAL_MEMORY_FULL         = 0x0080;
 
-	/** Memoria fiscal a punto de llenarse. La memoria fiscal tiene 30 o menos registros libres. Este bit debe ser monitoreado por la aplicaciÛn para emitir el correspondiente aviso. (Bit 8 de estado fiscal) */
+	/** Memoria fiscal a punto de llenarse. La memoria fiscal tiene 30 o menos registros libres. Este bit debe ser monitoreado por la aplicaci√≥n para emitir el correspondiente aviso. (Bit 8 de estado fiscal) */
 	public int FST_FISCAL_MEMORY_ALMOST_FULL  = 0x0100;
 
 	/** Terminal fiscal certificada. Indica que la impresora ha sido inicializada. (Bit 9 de estado fiscal) */
@@ -263,10 +263,10 @@ public interface HasarConstants
 	/** Terminal fiscal fiscalizada. Indica que la impresora ha sido inicializada. (Bit 10 de estado fiscal) */
 	public int FST_DEVICE_FISCALIZED          = 0x0400;
 
-	/** Error en ingreso de fecha. Se ha ingresado una fecha inv·lida. Para volver el bit a 0 debe ingresarse una fecha v·lida. (Bit 11 de estado fiscal) */
+	/** Error en ingreso de fecha. Se ha ingresado una fecha inv√°lida. Para volver el bit a 0 debe ingresarse una fecha v√°lida. (Bit 11 de estado fiscal) */
 	public int FST_CLOCK_ERROR                = 0x0800;
 
-	/** Documento fiscal abierto. Este bit se encuentra en 1 siempre que un documento fiscal (factura, recibo oficial o nota de crÈdito) se encuentra abierto. (Bit 12 de estado fiscal) */
+	/** Documento fiscal abierto. Este bit se encuentra en 1 siempre que un documento fiscal (factura, recibo oficial o nota de cr√©dito) se encuentra abierto. (Bit 12 de estado fiscal) */
 	public int FST_FISCAL_DOCUMENT_OPEN       = 0x1000;
 
 	/** Documento abierto (solo impresoras que soportan STATPRN). Este bit se encuentra en 1 siempre que un documento (fiscal, no fiscal o no fiscal homologado) se encuentra abierto. (Bit 13 de estado fiscal) */
@@ -281,6 +281,6 @@ public interface HasarConstants
 	/** Documento abierto en impresora Slip (solo impresoras que no soportan STATPRN). (Bit 14 de estado fiscal) */
 	public int FST_DOCUMENT_OPEN_SLIP         = 0x4000;
 
-	/** Suma lÛgica (OR) de los bits 0 a 8. Este bit se encuentra en 1 siempre que alguno de los bits mencionados se encuentre en 1. (Bit 15 de estado fiscal) */
+	/** Suma l√≥gica (OR) de los bits 0 a 8. Este bit se encuentra en 1 siempre que alguno de los bits mencionados se encuentre en 1. (Bit 15 de estado fiscal) */
 	public int FST_BITWISE_OR                 = 0x8000;
 }
